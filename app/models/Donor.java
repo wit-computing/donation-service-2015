7317357deda2e1354ff3c7c380bd0e6f0ccf6385
Eamonn de Leastar <edeleastar@wit.ie>
2015-11-10 16:54:12 +0000

    introduce donor-donation relationship

--- app/models/Donor.java
+++ app/models/Donor.java
@@ -3,6 +3,8 @@ package models;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import play.db.jpa.Model;
@@ -15,6 +17,9 @@ public class Donor extends Model
   public String email;
   public String password;
 
+  @OneToMany(cascade = CascadeType.ALL)
+  public List<Donation> donations = new ArrayList<Donation>();
+  
   public Donor(String firstName, String lastName, String email, String password)
   {
     this.firstName = firstName;
